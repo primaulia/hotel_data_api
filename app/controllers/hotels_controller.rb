6d2aba8a2f9deac1d5859fa16c38cf3a747ea473
Prima Aulia Gusta <prima@tablevibe.co>
2024-03-27 15:25:20 +0800

    add simple filter for the hotels results

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,5 +1,15 @@
 class HotelsController < ApplicationController
   def index
     @hotels = Hotel.order(created_at: :desc)
+
+    if params[:hotels].present?
+      slugs = params[:hotels].split(',')
+      @hotels = @hotels.where(slug: slugs)
+    end
+
+    return if params[:destinations].blank?
+
+    destination_ids = params[:destinations].split(',')
+    @hotels = @hotels.where(destination_id: destination_ids)
   end
 end
