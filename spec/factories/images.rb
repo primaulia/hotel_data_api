7b038e1a91c25f6626e9f660970bc7802750ad4e
Prima Aulia Gusta <prima@tablevibe.co>
2024-03-28 11:36:45 +0800

    update unit test for hotel

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :image do
     image_type { 'rooms' }
-    link { 'https://d2ey9sqrvkqdfs.cloudfront.net/Sjym/i93_m.jpg' }
+    sequence(:link) { |n| "https://d2ey9sqrvkqdfs.cloudfront.net/Sjym/i#{n}_m.jpg" }
     description { 'Double room' }
     hotel
   end
--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -13,4 +13,34 @@ RSpec.describe Hotel, type: :model do
     it { should have_many(:images) }
     it { should have_many(:booking_conditions) }
   end
+
+  describe '#amenities_list' do
+    let(:hotel) { create(:hotel) }
+    let!(:aircon) { create(:amenity, hotel:, name: 'aircon', amenity_type: 'room') }
+    let!(:pool) { create(:amenity, hotel:, name: 'outdoor pool', amenity_type: 'general') }
+    let!(:playground) { create(:amenity, hotel:, name: 'playground', amenity_type: 'general') }
+
+    it 'should return a hash of amenity list grouped by type' do
+      expect(hotel.amenities_list.keys).to match_array(%w[room general])
+      expect(hotel.amenities_list['general'].size).to eq(2)
+      expect(hotel.amenities_list['general']).to include('outdoor pool')
+      expect(hotel.amenities_list['room'].size).to eq(1)
+      expect(hotel.amenities_list['room']).to include('aircon')
+    end
+  end
+
+  describe '#images_list' do
+    let(:hotel) { create(:hotel) }
+    let!(:room1) { create(:image, hotel:, description: 'Double room', image_type: 'rooms') }
+    let!(:room2) { create(:image, hotel:, description: 'Single room', image_type: 'rooms') }
+    let!(:site) { create(:image, hotel:, description: 'Front', image_type: 'site') }
+
+    it 'should return a hash of images list grouped by type' do
+      expect(hotel.images_list.keys).to match_array(%w[rooms site])
+      expect(hotel.images_list['rooms'].size).to eq(2)
+      expect(hotel.images_list['rooms'].pluck(:description)).to include('Double room')
+      expect(hotel.images_list['site'].size).to eq(1)
+      expect(hotel.images_list['site'].pluck(:description)).to include('Front')
+    end
+  end
 end
